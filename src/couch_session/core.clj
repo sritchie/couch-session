145e5b71c4931e708c75bfd88c30865be9e56c7b
ken restivo <krestivo@restivo.org>
2012-05-23 20:28:51 -0700

    Make compatible with new clutch put-document keyword semantics. Also work around clutch returning database status instead of nil if key is empty.

--- src/couch_session/core.clj
+++ src/couch_session/core.clj
@@ -4,25 +4,31 @@
         ring.middleware.session.store)
   (:require [com.ashafa.clutch :as c]))
 
+(defn clutch-hack
+  "Workaround for clutch, which returns a id-less document with database status
+   when you do (get-document nil)."
+  [key]
+  (if (empty? key) nil (c/get-document key)))
+
 (deftype CouchStore [db]
   SessionStore
   (read-session [_ session-key]
-    (-> (c/with-db db (c/get-document session-key))
+    (-> (c/with-db db (clutch-hack session-key))
         (decode)))
 
   (write-session [_ session-key data]
     (c/with-db db
-      (:_id (let [doc (c/get-document session-key)
+      (:_id (let [doc (clutch-hack session-key)
                   data (-> data
                            (encode)
                            (merge (select-keys doc [:_id :_rev])))]
-              (cond (not doc) (c/put-document data session-key)
+              (cond (not doc) (c/put-document data :id session-key)
                     (or (= data {}) (= data doc)) doc
                     :else (c/put-document data))))))
   
   (delete-session [_ session-key]
     (c/with-db db
-      (when-let [doc (c/get-document session-key)]
+      (when-let [doc (clutch-hack session-key)]
         (c/delete-document doc)))
     nil))
 
